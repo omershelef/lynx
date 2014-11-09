5e50c6b6c9672f77b41bb03208e796c705cc19d2
omershelef <shlaflaf@gmail.com>
2014-11-09 19:39:23 +0200

    Update setup.py

--- lynx.py
+++ lynx.py
@@ -1,6 +1,6 @@
 import re
 
-__version__ = '0.1'
+__version__ = '0.2'
 __all__ = [
     'load', 'loads', 'Section'
 ]
--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from distutils.core import setup
+import os
+
+
 setup(name='lynx',
-      version='0.1',
+      version='0.2.1',
+      description = "Python configuration library",
+      license="The MIT License (MIT)",
+      url = "https://github.com/omershelef/lynx/",
+      author = "Omer Shelef",
+      author_email = "shlaflaf@gmail.com",
       py_modules=['lynx'],
       )
